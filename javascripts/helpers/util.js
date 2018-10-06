a5d3fb214019543a2e1b037bbd874c09a56eed57
Marshall Offutt <marshalloffutt@gmail.com>
2018-10-06 13:30:04 -0500

    Adds setup files

---
+++ javascripts/helpers/util.js
@@ -0,0 +1,8 @@
+console.log('cominatcha from util.js');
+
+const printToDom = (stringToPrint, divId) => {
+    selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+  };
+
+  export { printToDom }
